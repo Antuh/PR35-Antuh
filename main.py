4935d9ba54c1534b69c05bc75848bc3094c096fe
Antuh <antuhsergey648@gmail.com>
2023-02-04 22:00:52 +0700

    Initial one commit

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from kivy.uix.widget import Widget
 from kivy.properties import (
     NumericProperty, ReferenceListProperty, ObjectProperty
 )
+from kivy.config import Config
+Config.set('graphics', 'resizable', '0')
+Config.set('graphics', 'width', '360')
+Config.set('graphics', 'height', '800')
 from kivy.vector import Vector
 from kivy.clock import Clock
-
-
 class PongPaddle(Widget):
     score = NumericProperty(0)
 
@@ -14,9 +16,9 @@ class PongPaddle(Widget):
         if self.collide_widget(ball):
             vx, vy = ball.velocity
             offset = (ball.center_y - self.center_y) / (self.height / 2)
-            bounced = Vector(-1 * vx, vy)
+            bounced = Vector( -1* vx,  vy)
             vel = bounced * 1.1
-            ball.velocity = vel.x, vel.y + offset
+            ball.velocity = vel.y, vel.x + offset
 
 
 class PongBall(Widget):
@@ -33,7 +35,7 @@ class PongGame(Widget):
     player1 = ObjectProperty(None)
     player2 = ObjectProperty(None)
 
-    def serve_ball(self, vel=(4, 0)):
+    def serve_ball(self, vel=(0, 4)):
         self.ball.center = self.center
         self.ball.velocity = vel
 
@@ -45,29 +47,33 @@ class PongGame(Widget):
         self.player2.bounce_ball(self.ball)
 
         # bounce ball off bottom or top
-        if (self.ball.y < self.y) or (self.ball.top > self.top):
-            self.ball.velocity_y *= -1
+        if (self.ball.y < self.y):
 
+            self.serve_ball(vel=(0, 2))
+            self.player1.score += 1
+        if (self.ball.top > self.top):
+            self.serve_ball(vel=(0, -2))
+            
+            self.player2.score += 1
         # went of to a side to score point?
         if self.ball.x < self.x:
-            self.player2.score += 1
-            self.serve_ball(vel=(4, 0))
+            self.ball.velocity_x *= -1
+            
         if self.ball.right > self.width:
-            self.player1.score += 1
-            self.serve_ball(vel=(-4, 0))
+            self.ball.velocity_x *= -1
 
     def on_touch_move(self, touch):
-        if touch.x < self.width / 3:
-            self.player1.center_y = touch.y
-        if touch.x > self.width - self.width / 3:
-            self.player2.center_y = touch.y
+        if touch.y < self.width / 3:
+            self.player1.center_x = touch.x
+        if touch.y > self.width - self.width / 3:
+            self.player2.center_x = touch.x
 
 
 class PongApp(App):
     def build(self):
         game = PongGame()
         game.serve_ball()
-        Clock.schedule_interval(game.update, 1.0 / 60.0)
+        Clock.schedule_interval(game.update, 1.0 / 120.0)
         return game
 
 
